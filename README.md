3c87639d19d7b6d05b92f906fecf70153081b3ad
maskedmarker <changjianghui0822@gmail.com>
2024-10-17 12:42:40 +0800

    webflux与servlet-mvc还是有许多不同

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # spring-boot-webflux-learn
+
+
+## 注意
+
+1. webflux不依赖servlet
+2. 因为不依赖servlet,所以配置文件中的server.servlet是无效的
+3. druid中与servlet相关的都不能配置,否则报异常.
+4. 
--- ch01-hello/src/main/java/org/example/learn/spring/boot/webflux/hello/controller/UserController.java
+++ ch01-hello/src/main/java/org/example/learn/spring/boot/webflux/hello/controller/UserController.java
@@ -8,22 +8,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
     @Autowired
     private UserService userService;
 
     @GetMapping("/getAllUsers")
-    public List<User> getAllUsers() {
-        return userService.findAllUsers();
+    public Flux<User> getAllUsers() {
+        return Flux.just(userService.findAllUsers().toArray(new User[0]));
     }
 
     @PostMapping("/createUser")
-    public User createUser(@RequestBody User user) {
-        return userService.saveUser(user);
+    public Mono<User> createUser(@RequestBody User user) {
+        return Mono.just(userService.saveUser(user));
     }
 }
